d0b9ba5b00f3c5fbc0c751fb9a6efae4b6470073
Fazo <fazius2009@gmail.com>
2013-08-24 09:17:49 +0200

    Risolto bug: tutte le stringhe considerate invalide

--- NetLibz/src/utilz/Utils.java
+++ NetLibz/src/utilz/Utils.java
@@ -15,10 +15,16 @@ public class Utils {
     public static int nanoToSec(Long n) { //fa una semplice divisione: da nanoscondi a secondi
         return (int) (n / 1000000000);
     }
-public static boolean isValid(String s){
-    for(char c:s.toCharArray())if(c!=' ')return true;
-    return false;
-}
+
+    public static boolean isValid(String s) {
+        for (char c : s.toCharArray()) {
+            if (c != ' ') {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static ArrayList<String> toList(String s, String regex) { //passa da una stringa a una lista.
         if (regex == null) {
             regex = " ";
@@ -33,18 +39,26 @@ public static boolean isValid(String s){
         }
         return content;
     }
-public static void mergeLists(ArrayList<String> a, ArrayList<String> b){
-    //copia il contenuto di B in a
-    for(String c:b){
-        if(!a.contains(c))a.add(c);
+
+    public static void mergeLists(ArrayList<String> a, ArrayList<String> b) {
+        //copia il contenuto di B in a
+        for (String c : b) {
+            if (!a.contains(c)) {
+                a.add(c);
+            }
+        }
     }
-}
+
     public static String fromList(ArrayList<String> ss, String divisor) { //passa da una lista a una stringa
-        if(ss==null)return null;
-        if(divisor==null)divisor=" ";
-        String a=ss.get(0);
-        for(int i=1;i<ss.size();i++){
-            a+=divisor+ss.get(i);
+        if (ss == null) {
+            return null;
+        }
+        if (divisor == null) {
+            divisor = " ";
+        }
+        String a = ss.get(0);
+        for (int i = 1; i < ss.size(); i++) {
+            a += divisor + ss.get(i);
         }
         return a;
     }
