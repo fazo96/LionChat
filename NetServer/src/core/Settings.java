a374db00d17f54cc7a0a5831ac8d04a3df549518
Fazo <fazius2009@gmail.com>
2013-08-24 10:25:09 +0200

    Aggiunta stringa "status"

--- NetServer/src/core/Settings.java
+++ NetServer/src/core/Settings.java
@@ -15,6 +15,7 @@ public class Settings {
 
     private static int port;
     private static String helpMsg, adminHelpMsg, motd;
+    public static String status="";
     public static Group groupGuest, groupUser, groupAdmin;
     private static boolean init = false;
 
